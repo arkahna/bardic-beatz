1f622cf72f80bf4060b3ac74cf2341d8db9b8643
Emlyn Rice <emlyn.rice@arkahna.io>
2024-02-15 08:49:00 +0800

    Remove comments

--- apps/webapp/app/lib/config.server.ts
+++ apps/webapp/app/lib/config.server.ts
@@ -9,9 +9,7 @@ export const {
 } = z
     .object({
         NODE_ENV: z.string().default('development'),
-        // ENVIRONMENT_NAME: z.string(),
         SESSION_SECRET: z.string().default('SESSION_SECRET'),
-        // WEB_URL: z.string().lo,
 
         // These are used by the web application for authentication with spotify
         SPOTIFY_CLIENT_ID: z.string(),
@@ -21,5 +19,3 @@ export const {
         FEATUREBOARD_ENVIRONMENT_APIKEY: z.string(),
     })
     .parse(process.env)
-
-// export const FULL_WEB_URL = `${process.env.NODE_ENV === 'production' ? 'https://' : 'http://'}${WEB_URL}`
